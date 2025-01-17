3db0e99deb0c3f5dd34d5e83e8a211c0b7ff67e2
AKhil <akhilacharya343@gmail.com>
2025-01-18 01:52:57 +0530

    first commit

---
+++ Client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>
+        Project Banao ji
+      </h1>
+    </div>
+  );
+}
