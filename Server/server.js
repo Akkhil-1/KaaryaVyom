3ccfc814013ab0d44f006ab884429fa6add3f06d
AKhil <akhilacharya343@gmail.com>
2025-01-18 01:33:45 +0530

    chnge to js

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,14 @@
-import express from 'express';
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT;
 app.use(express.json());
-app.get('/' , (req,res) => {
-    res.json({
-        message: "Hello World!",
-        status : 200
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World!",
+    status: 200,
+  });
 });
-app.listen(port , () => {
-    console.log(`Running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
