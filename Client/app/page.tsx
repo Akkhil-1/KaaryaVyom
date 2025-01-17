f59de01c69d2b71db324d2e5a065479e34370121
aryan138 <apathania206@gmail.com>
2025-01-17 21:05:37 +0530

    haha

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return(
     <div>
-      <h1 className="text-center">Project Banao</h1>
+      <h1 className="text-center">Project Banao ji</h1>
     </div>
   )
 }
